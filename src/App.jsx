687068f403fb6b7ba5e8d195a9f7fc11e9457d86
WillyDevHub <guillermobiletto99@gmail.com>
2025-08-12 16:16:06 -0300

    test 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header/Header"
+import {Header} from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Main from "./components/Main/Main"
 import Aside from "./components/Aside/Aside"
